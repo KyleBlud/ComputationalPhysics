0894b08a67d1260525e9adac3dacd0ed11802f9d
KyleBlud <kylebludworth@outlook.com>
2017-09-16 15:12:13 -0400

    Complete decay chain implemented
    All that’s left to is graph the numbers!

--- Assignment2/RadioactiveDecayChain/RDC.py
+++ Assignment2/RadioactiveDecayChain/RDC.py
@@ -14,6 +14,7 @@ def p(half_t):
 Bi213_HALF_LIFE = 2760         # 46 minutes in seconds
 Tl209_HALF_LIFE = 132          # 2.2 minutes in seconds
 Pb209_HALF_LIFE = 198          # 3.3 minutes in seconds
+TOTAL_TIME = 20000
 
 Bi213_total = 10000
 Tl209_total = 0
@@ -23,10 +24,24 @@ Bi209_total = 0
 decay_rand = 0
 route_rand = 0
 
-for t in range(1, 2671):
+for t in range(1, TOTAL_TIME + 1):
+    # Pb209
+    init_Pb209_total = Pb209_total
+    for atom in range(init_Pb209_total):
+        decay_rand = r.uniform(0, 1)
+        if (decay_rand <= p(Pb209_HALF_LIFE)):
+            Pb209_total -= 1
+            Bi209_total += 1
+    # Tl209
+    init_Tl209_total = Tl209_total
+    for atom in range(init_Tl209_total):
+        decay_rand = r.uniform(0, 1)
+        if (decay_rand <= p(Tl209_HALF_LIFE)):
+            Tl209_total -= 1
+            Pb209_total += 1
     # Bi213 
     init_Bi213_total = Bi213_total
-    for atom in range(1, init_Bi213_total):
+    for atom in range(init_Bi213_total ):
         decay_rand = r.uniform(0, 1)
         if (decay_rand <= p(Bi213_HALF_LIFE)):
             Bi213_total -= 1
@@ -35,4 +50,8 @@ for t in range(1, 2671):
                 Pb209_total += 1
             else:
                 Tl209_total += 1
-          
+
+print("Bi213 Total: " + str(Bi213_total))
+print("Pb209 Total: " + str(Pb209_total))
+print("Tl209 Total: " + str(Tl209_total))
+print("Bi209 Total: " + str(Bi209_total))
