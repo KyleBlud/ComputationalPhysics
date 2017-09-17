abc0150e9dbff0920a5ed3f8e797b00e8673eea0
KyleBlud <kylebludworth@outlook.com>
2017-09-16 23:45:04 -0400

    Plot radioactive decay of 213Bi isotope

--- Assignment2/RadioactiveDecayChain/RDC.py
+++ Assignment2/RadioactiveDecayChain/RDC.py
@@ -4,8 +4,8 @@ Physics 420
 September 9, 2017
 '''
 import math as m
-import numpy as np
 import random as r
+import matplotlib.pyplot as plt
 
 # Probability of a given isotope with half-life "half_t" to decay
 def p(half_t):
@@ -20,6 +20,12 @@ Bi213_total = 10000
 Tl209_total = 0
 Pb209_total = 0
 Bi209_total = 0
+
+Bi213_vals = [10000]
+Tl209_vals = [0]
+Pb209_vals = [0]
+Bi209_vals = [0]
+time = [0]
 
 decay_rand = 0
 route_rand = 0
@@ -41,7 +47,7 @@ for t in range(1, TOTAL_TIME + 1):
             Pb209_total += 1
     # Bi213 
     init_Bi213_total = Bi213_total
-    for atom in range(init_Bi213_total ):
+    for atom in range(init_Bi213_total):
         decay_rand = r.uniform(0, 1)
         if (decay_rand <= p(Bi213_HALF_LIFE)):
             Bi213_total -= 1
@@ -50,8 +56,19 @@ for t in range(1, TOTAL_TIME + 1):
                 Pb209_total += 1
             else:
                 Tl209_total += 1
+    Bi213_vals.append(Bi213_total)
+    Tl209_vals.append(Tl209_total)
+    Pb209_vals.append(Pb209_total)
+    Bi209_vals.append(Bi209_total)
+    time.append(t)
 
-print("Bi213 Total: " + str(Bi213_total))
-print("Pb209 Total: " + str(Pb209_total))
-print("Tl209 Total: " + str(Tl209_total))
-print("Bi209 Total: " + str(Bi209_total))
+plt.style.use('dark_background')
+plt.plot(time, Bi213_vals, label = "Bi213")
+plt.plot(time, Tl209_vals, label = "Tl209")
+plt.plot(time, Pb209_vals, label = "Pb209")
+plt.plot(time, Bi209_vals, label = "Bi209")
+plt.xlabel("Time (s)")
+plt.ylabel("Atoms")
+plt.title("Radioactive Decay of 213Bi Isotope")
+plt.legend()
+plt.show()
