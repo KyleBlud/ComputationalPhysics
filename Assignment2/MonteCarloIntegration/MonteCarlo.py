25a87aeac62107276a818b86794699cb30502e54
KyleBlud <kylebludworth@outlook.com>
2017-09-19 14:49:31 -0400

    Change probability distribution

--- Assignment2/MonteCarloIntegration/MonteCarlo.py
+++ Assignment2/MonteCarloIntegration/MonteCarlo.py
@@ -16,8 +16,8 @@ def p(x):
 def monte_carlo(n, partB = False):
     summation = 0
     for i in range(1, n + 1):
-        x = r.uniform(0, 1)
-        summation += f(x) * p(x) if partB else f(x)
+        x = r.uniform(0, 1)**2 if partB else r.uniform(0, 1)
+        summation += f(x) / p(x) if partB else f(x)
     estimate = summation / n
     return estimate
 
@@ -29,12 +29,10 @@ n_vals.append(10)
 p1_vals.append(monte_carlo(10))
 p2_vals.append(monte_carlo(10, True))
 
-for n in range(100, 1000000, 100):
+for n in range(100, 100000, 100):
     n_vals.append(n)
     p1_vals.append(monte_carlo(n))
     p2_vals.append(monte_carlo(n, True))
-
-print("Done")
 
 plt.style.use('dark_background')
 plt.plot(n_vals, p1_vals, label = "p(x) = 1")
@@ -42,7 +40,7 @@ plt.plot(n_vals, p2_vals, label = "p(x) = 1/2sqrt(x)")
 plt.axhline(y = 0.83893296, label = "Actual", color = "red", linestyle = "--")
 plt.xscale("log")
 plt.xlabel("Number of Samples")
-plt.ylabel("Integral values")
-#plt.title("Radioactive Decay of 213Bi Isotope")
+plt.ylabel("Integral Values")
+plt.title("Monte Carlo Approximation")
 plt.legend()
 plt.show()
