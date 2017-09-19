99403865d186b92288feb3b60e7c825b87c0c7b8
KyleBlud <kylebludworth@outlook.com>
2017-09-19 13:12:55 -0400

    Created bounds for the lattice

--- Assignment2/BrownianMotion/BrownianMotion.py
+++ Assignment2/BrownianMotion/BrownianMotion.py
@@ -6,17 +6,27 @@ Creation date: September 16, 2017
 import random as r
 import turtle as t
 
-L = 101
+# Maginfied by 5 to see the movement through the lattice
+L = 101 * 5
 
 wn = t.Screen()
-wn.screensize(L, L)
+wn.setup(width = L, height = L)
 
 jim = t.Turtle()
 jim.hideturtle()
 jim.speed("fastest")
 
-directions = [0, 90, 180, 270]
+directions = [(5, 0), (-5, 0), (0, 5), (0, -5)]
+
+x = 0
+y = 0
 
 for i in range(1000000):
-    jim.setheading(r.choice(directions))
-    jim.forward(1)
+    (dx, dy) = r.choice(directions)
+    while (jim.xcor() + dx > int(L/2) or jim.xcor() + dx < int(-L/2) or
+           jim.ycor() + dy > int(L/2) or jim.ycor() + dy < int(-L/2)):
+        (dx, dy) = r.choice(directions)
+    x += dx
+    y += dy
+    jim.goto(x, y)
+    
